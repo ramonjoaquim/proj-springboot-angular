8c1588a4bfcfd63b00f41a94790a24c5d5cf76d8
Ramon Joaquim <39497210+ramonjoaquim@users.noreply.github.com>
2020-08-23 23:05:02 -0300

    Update init-database.sh

--- init-database.sh
+++ init-database.sh
@@ -5,6 +5,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER docker;
     CREATE DATABASE dcc;
     GRANT ALL PRIVILEGES ON DATABASE dcc TO docker;
-    CREATE DATABASE my_project_test;
-    GRANT ALL PRIVILEGES ON DATABASE dcc TO docker;
 EOSQL
